30f0ed21461b6edbaa180e54fe9eae3cd73c04ea
eliel358 <elielhenrique360flip@gmail.com>
2024-02-17 16:44:42 -0300

    Implentando obrigatoriedade de informações na devolução

--- js/devolution.js
+++ js/devolution.js
@@ -1,3 +1,11 @@
 document.getElementById('goto_home').addEventListener('click',()=>{
   document.location.href='../index.html'
 })
+document.getElementById('devolution').addEventListener('click',()=>{
+  if(document.getElementById('register_devolution').value == ''){
+  
+    alert('Atenção, preencha todos os campos para devolver o livro')
+  }
+  console.log(document.getElementById('register_devolution').value)
+
+})
