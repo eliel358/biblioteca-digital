0a90375107407f723e5269ab48ccddc5801dca3a
eliel358 <elielhenrique360flip@gmail.com>
2024-03-01 13:23:58 -0300

    Dark theme na pagina inicial

--- js/home.js
+++ js/home.js
@@ -14,3 +14,32 @@ document.getElementById('goto_devolution').addEventListener('click',()=>{
 document.getElementById('goto_book_list').addEventListener('click',()=>{
   document.location.href = './pages/book_list.html'
 })
+
+document.getElementById('theme-change').addEventListener('click',()=>{
+
+  if(document.getElementById('theme-change').value == 'white'){
+
+    document.getElementById('theme-change').innerHTML = 'Tema claro'
+    document.getElementById('theme-change').value = 'dark'
+    
+    document.getElementById('body').className = 'body-dark'
+    
+    document.getElementById('goto_add_book').className = 'button-dark non-border'
+    document.getElementById('goto_remove_book').className = 'button-dark non-border'
+    document.getElementById('goto_loan').className = 'button-dark non-border'
+    document.getElementById('goto_devolution').className = 'button-dark non-border'
+    document.getElementById('goto_book_list').className = 'button-dark non-border'
+    document.getElementById('theme-change').className = 'theme-button button-dark'
+  }else{
+    
+    document.getElementById('theme-change').innerHTML = 'Tema escuro'
+    document.getElementById('theme-change').className = 'theme-button'
+    document.getElementById('body').className = 'body'
+    document.getElementById('theme-change').value = 'white'
+    document.getElementById('goto_add_book').className = 'non-border'
+    document.getElementById('goto_remove_book').className = 'non-border'
+    document.getElementById('goto_loan').className = 'non-border'
+    document.getElementById('goto_devolution').className = 'non-border'
+    document.getElementById('goto_book_list').className = 'non-border'
+  }
+})
