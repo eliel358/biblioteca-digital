2b6e1711f6af72a399010711c04c7ba6bea89c3a
eliel358 <elielhenrique360flip@gmail.com>
2024-03-20 22:12:05 -0300

    Devolver livro não remove o livro da db, apenas muda o status

--- js/devolution.js
+++ js/devolution.js
@@ -17,22 +17,23 @@ const serviceAccountAuth  = new JWT.JWT({
 
 const doc = new GoogleSpreadsheet.GoogleSpreadsheet(RESPONSES_SHEET_ID,serviceAccountAuth)
 
-const deleteRow = async (target) => {
+const updateRow = async (register, oldValue, newValue) => {
 
-  target = String(target) 
   await doc.loadInfo();
-
+  register = String(register)
   let sheet = doc.sheetsByIndex[0];
 
   let rows = await sheet.getRows();
+    
   for(i in rows){
-  
-    if(rows[i]['_rawData'][1] === target){
-      await rows[i].delete()
-      alert('O livro "'+rows[i]['_rawData'][0]+'" foi excluido')
-      return
-    }else{
-      alert('O livro de registro ' +target+ ' já havia sido devolvido ou não foi encontrado')
+    if(rows[i]['_rawData'][1] === register){
+      for(a in rows[i]['_rawData']){
+        if(rows[i]['_rawData'][a] === oldValue){
+          rows[i]['_rawData'][a] = newValue
+          await rows[i].save();
+          return
+        }
+      }
     }
   }
 };
@@ -41,6 +42,5 @@ document.getElementById('devolution').addEventListener('click',()=>{
     alert('Atenção, preencha todos os campos para devolver o livro')
   }
   console.log(document.getElementById('register_devolution').value)
-  deleteRow(document.getElementById('register_devolution').value)
-
+  updateRow(document.getElementById('register_devolution').value,"Emprestado","Em estoque")
 })
