adbc97d2d5be71da0b33780c697a2d8e0845e8f3
eliel358 <elielhenrique360flip@gmail.com>
2024-04-21 21:45:15 -0300

    atualizando getTable

--- js/api.js
+++ js/api.js
@@ -23,9 +23,11 @@ const getTable = async () => {
     let sheet = doc.sheetsByIndex[0];
 
     let rows = await sheet.getRows();
-    for(let i = 0; i < rows.length;i++){
-        console.log(rows[i]['_rawData'])
+    array = []
+    for(i in rows){
+      array.push(rows[i]['_rawData'])
     }
+    console.table(array)
 };
 
 const getBookbyregister = async (register) => {
@@ -108,17 +110,3 @@ const addRow = async (rows) => {
   }
   
 };
-
-x = [
-book1 = new book('Dom Casmurro', 101, 'Machado de Assis', 'Ana', '2A', 'Tarde', 'Manhã', '28/02/2024', '07/03/2024'),
-book2 = new book('1984', 102, 'George Orwell', 'Carlos', '3B', 'Manhã', 'Tarde', '01/03/2024', '08/03/2024'),
-book3 = new book('Orgulho e Preconceito', 103, 'Jane Austen', 'Mariana', '1C', 'Noite', 'Manhã', '02/03/2024', '09/03/2024'),
-book4 = new book('O Senhor dos Anéis', 104, 'J.R.R. Tolkien', 'Lucas', '4A', 'Manhã', 'Tarde', '03/03/2024', '10/03/2024'),
-book5 = new book('Harry Potter e a Pedra Filosofal', 105, 'J.K. Rowling', 'Isabela', '2B', 'Tarde', 'Manhã', '04/03/2024', '11/03/2024'),
-book6 = new book('A Revolução dos Bichos', 106, 'George Orwell', 'Mateus', '3C', 'Manhã', 'Tarde', '05/03/2024', '12/03/2024'),
-book7 = new book('Crime e Castigo', 107, 'Fiódor Dostoiévski', 'Camila', '1A', 'Noite', 'Manhã', '06/03/2024', '13/03/2024'),
-book8 = new book('Cem Anos de Solidão', 108, 'Gabriel García Márquez', 'Rodrigo', '4B', 'Manhã', 'Tarde', '07/03/2024', '14/03/2024'),
-book9 = new book('O Pequeno Príncipe', 109, 'Antoine de Saint-Exupéry', 'Laura', '2C', 'Tarde', 'Manhã', '08/03/2024', '15/03/2024'),
-book10 = new book('O Hobbit', 110, 'J.R.R. Tolkien', 'Gabriel', '3A', 'Manhã', 'Tarde', '09/03/2024', '16/03/2024')
-]
-addRow(x)
